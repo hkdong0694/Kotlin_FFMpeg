a53f30f692ccf413e3369046d4162fb7557c5e69
한경동 <hkd0694@gmail.com>
2021-08-17 00:15:37 +0900

    FFMpeg Mobile 라이브러리 추가

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,4 +1,16 @@
-//
-// Created by kyungdong on 03/08/2021.
-//
+#include <jni.h>
+#include <string>
+// jstring -> JNI 에서 사용하는 데이터 구조, 자바 문자열의 포인터를 가리키는 포인터 타입이다.
 
+extern "C" {
+#include "libavformat/avformat.h"
+}
+
+extern "C" jstring
+Java_com_example_ffmpegsample_MainActivity_stringFromJNI(JNIEnv* env,jobject ) {
+    std::string hello = "Hello from C++";
+
+    av_register_all();
+
+    return env->NewStringUTF(hello.c_str());
+}
--- app/src/main/java/com/example/ffmpegsample/MainActivity.kt
+++ app/src/main/java/com/example/ffmpegsample/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.ffmpegsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.arthenica.mobileffmpeg.FFmpeg
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var id = FFmpeg.execute("")
+
     }
 }
