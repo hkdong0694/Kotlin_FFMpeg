25ece8d249830bd07dc92b3afa24b1f8b7afc21c
한경동 <hkd0694@gmail.com>
2021-08-21 16:26:26 +0900

    이미지 파일 및 FileUtils 코틀린 변환

---
+++ app/src/main/java/com/example/ffmpegsample/FileUtils.kt
@@ -0,0 +1,153 @@
+package com.example.ffmpegsample
+
+import android.content.Context
+import android.database.Cursor
+import android.net.Uri
+import android.os.Environment
+import android.provider.DocumentsContract
+import android.provider.MediaStore
+import java.io.File
+
+
+/**
+ * FFMpegSample
+ * Class: FileUtils
+ * Created by 한경동 (Joel) on 2021/08/21.
+ * Description:
+ */
+object FileUtils {
+
+    // UPDATED!
+    fun getPath(context: Context, uri: Uri?): String? {
+        val projection = arrayOf(MediaStore.Video.Media.DATA)
+        val cursor: Cursor? =
+            uri?.let { context.getContentResolver().query(it, projection, null, null, null) }
+        return if (cursor != null) {
+            // HERE YOU WILL GET A NULLPOINTER IF CURSOR IS NULL
+            // THIS CAN BE, IF YOU USED OI FILE MANAGER FOR PICKING THE MEDIA
+            val column_index: Int = cursor
+                .getColumnIndexOrThrow(MediaStore.Video.Media.DATA)
+            cursor.moveToFirst()
+            cursor.getString(column_index)
+        } else null
+    }
+
+    /**
+     * Get a file from a Uri.
+     * Framework Documents, as well as the _data field for the MediaStore and
+     * other file-based ContentProviders.
+     *
+     * @param context The context.
+     * @param uri     The Uri to query.
+     */
+    fun getFileFromUri(context: Context, uri: Uri): File? {
+        var path: String? = null
+
+        // DocumentProvider
+        if (DocumentsContract.isDocumentUri(context, uri)) {
+
+
+            // ExternalStorageProvider
+            if (isExternalStorageDocument(uri)) {
+                val docId = DocumentsContract.getDocumentId(uri)
+                val split = docId.split(":").toTypedArray()
+                val type = split[0]
+                if ("primary".equals(type, ignoreCase = true)) {
+                    path = Environment.getExternalStorageDirectory().toString() + "/" + split[1]
+                }
+
+                // TODO handle non-primary volumes
+            } else if (isDownloadsDocument(uri)) { // DownloadsProvider
+                path = getPath(context, uri)
+                val id = DocumentsContract.getDocumentId(uri)
+                //                final Uri contentUri = ContentUris.withAppendedId(
+//                        Uri.parse("content://downloads/public_downloads"), Long.parseLong(id));
+                path = getDataColumn(context, uri, null, null)
+            } else if (isMediaDocument(uri)) { // MediaProvider
+                val docId = DocumentsContract.getDocumentId(uri)
+                val split = docId.split(":").toTypedArray()
+                val type = split[0]
+                var contentUri: Uri? = null
+                if ("image" == type) {
+                    contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                } else if ("video" == type) {
+                    contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI
+                } else if ("audio" == type) {
+                    contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
+                }
+                val selection = "_id=?"
+                val selectionArgs = arrayOf(
+                    split[1]
+                )
+                path = getDataColumn(context, contentUri, selection, selectionArgs)
+            } // MediaStore (and general)
+        } else if ("content".equals(uri.getScheme(), ignoreCase = true)) {
+            path = getDataColumn(context, uri, null, null)
+        } else if ("file".equals(uri.getScheme(), ignoreCase = true)) {
+            path = uri.getPath()
+        }
+        return File(path)
+    }
+
+    /**
+     * Get the value of the data column for this Uri. This is useful for
+     * MediaStore Uris, and other file-based ContentProviders.
+     *
+     * @param context       The context.
+     * @param uri           The Uri to query.
+     * @param selection     (Optional) Filter used in the query.
+     * @param selectionArgs (Optional) Selection arguments used in the query.
+     * @return The value of the _data column, which is typically a file path.
+     */
+    fun getDataColumn(
+        context: Context, uri: Uri?, selection: String?,
+        selectionArgs: Array<String>?
+    ): String? {
+        var cursor: Cursor? = null
+        val column = MediaStore.Images.Media.DATA
+        val projection = arrayOf(
+            column
+        )
+        try {
+            cursor = uri?.let {
+                context.contentResolver.query(
+                    it, projection, selection, selectionArgs,
+                    null
+                )
+            }
+            if (cursor != null && cursor.moveToFirst()) {
+                val column_index: Int = cursor.getColumnIndexOrThrow(column)
+                return cursor.getString(column_index)
+            }
+        } finally {
+            if (cursor != null) cursor.close()
+        }
+        return null
+    }
+
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is ExternalStorageProvider.
+     */
+    fun isExternalStorageDocument(uri: Uri): Boolean {
+        return "com.android.externalstorage.documents" == uri.getAuthority()
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is DownloadsProvider.
+     */
+    fun isDownloadsDocument(uri: Uri): Boolean {
+        return "com.android.providers.downloads.documents" == uri.getAuthority()
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is MediaProvider.
+     */
+    fun isMediaDocument(uri: Uri): Boolean {
+        return "com.android.providers.media.documents" == uri.getAuthority()
+    }
+
+}
--- app/src/main/java/com/example/ffmpegsample/MainActivity.kt
+++ app/src/main/java/com/example/ffmpegsample/MainActivity.kt
@@ -8,11 +8,9 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
 
 
     }
