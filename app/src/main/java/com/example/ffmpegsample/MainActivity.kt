36a4afef7ed78d0c3128a631b6bbe693d82efc6d
한경동 <hkd0694@gmail.com>
2021-08-21 16:11:30 +0900

    라비르러리 추가 및 viewBinding 설정

--- app/src/main/java/com/example/ffmpegsample/MainActivity.kt
+++ app/src/main/java/com/example/ffmpegsample/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.ffmpegsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import com.example.ffmpegsample.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
     }
 }
